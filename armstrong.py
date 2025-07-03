f329f6e2db843c873fd9a969334bff3125f88a32
RadhikaKatta <devd83211@gmail.com>
2025-07-03 11:50:20 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number:"))
+temp=num
+n=len(str(num))
+sum=0
+while temp>0:
+    digit=temp%10
+    sum+=digit**n
+    temp//=10
+print("latest value of sum:",sum)
+print("latest value of temp:",temp)  
+if sum==num:
+    print("armstrong ")
+else:
+    print("not")    
+      
